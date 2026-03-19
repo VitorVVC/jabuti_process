7f79c189be7b03a6fb70f26bd70f16bcf6d496d8
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2026-03-19 16:37:23 -0300

    Add main.py

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+from app.api.routes.user import router as user_router
+from app.api.routes.user_test import router as user_router_test
+from app.core.config import settings
+from app.core.database import Base, engine
+
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+)
+
+app.include_router(user_router)
+app.include_router(user_router_test)
+
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def healthcheck():
+    return {
+        "message": "API is running",
+        "app_name": settings.APP_NAME,
+        "version": settings.APP_VERSION,
+    }
